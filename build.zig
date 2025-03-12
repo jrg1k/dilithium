2e896a702ce36d0ec398329a126c84e31668cc36
Jørgen Kristensen <jk@bitnor.org>
2025-03-12 15:30:37 +0100

    add build.zig
    add zig build system

---
+++ build.zig
@@ -0,0 +1,61 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const cflags = .{
+        "-Wall",
+        "-Werror",
+        "-Wextra",
+        "-Wmissing-prototypes",
+        "-Wno-unused-result",
+        "-Wpedantic",
+        "-Wpointer-arith",
+        "-Wredundant-decls",
+        "-Wshadow",
+        "-Wvla",
+    };
+
+    const cfiles = .{
+        "ntt.c",
+        "packing.c",
+        "poly.c",
+        "polyvec.c",
+        "reduce.c",
+        "rounding.c",
+        "sign.c",
+    };
+
+    const dilithium_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .omit_frame_pointer = true,
+        .single_threaded = true,
+        .strip = true,
+    });
+
+    dilithium_mod.addCSourceFiles(.{
+        .root = b.path("ref"),
+        .files = &.{ "fips202.c", "symmetric-shake.c" },
+        .flags = &cflags,
+    });
+
+    const modes = .{ "2", "3", "5" };
+
+    inline for (modes) |m| {
+        dilithium_mod.addCSourceFiles(.{
+            .root = b.path("ref"),
+            .files = &cfiles,
+            .flags = &(cflags ++ .{"-DDILITHIUM_MODE=" ++ m}),
+        });
+    }
+
+    const dylib = b.addLibrary(.{
+        .linkage = .dynamic,
+        .name = "pqcrystals_dilithium_ref",
+        .root_module = dilithium_mod,
+    });
+
+    b.installArtifact(dylib);
+}
